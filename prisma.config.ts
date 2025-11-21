7430502c92e429213e5ed67224e645aaeb81af57
Mahadevaprasad R <116330539+Mahadevaprasad1998@users.noreply.github.com>
2025-11-21 12:12:56 +0530

    Revert "Tenant Onboarding with Kratos Identity Integration"
    This reverts commit 55aa9010e0516f7eabb26459a637e9b765d8afb5.

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,4 @@
 import { defineConfig, env } from "prisma/config";
-import { PrismaClient } from '@prisma/client';
-
 import 'dotenv/config';
 export default defineConfig({
   schema: "prisma/schema.prisma",
